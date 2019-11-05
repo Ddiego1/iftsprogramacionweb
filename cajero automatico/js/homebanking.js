5abb71a11fec3482613113cb56bf4478f4a9159d
diego <diegogomezd98@gmail.com>
2019-11-05 16:17:03 -0300

    cambios al js del cajero 5/11

--- cajero automatico/js/homebanking.js
+++ cajero automatico/js/homebanking.js
@@ -15,7 +15,15 @@ window.onload = function() {
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+    var val1 =Number(prompt("Ingrese el limite de extraccion nuevo"));
+    if(val1 == isNaN || val1 <= 0 ){
+        alert("Ingresar un valor aceptado");
+    }
+    else{
+        limiteExtraccion = val1;
+        actualizarLimiteEnPantalla();
+        alert("El nuevo limite de extraccion es de " + val1);
+    }
 }
 
 function extraerDinero() {
@@ -24,8 +32,24 @@ function extraerDinero() {
         alert("Ingresar un valor aceptado");
     }
     else{
-        restarcuenta(val1);
-        alert("Se ha retirado: "+val1 +"\nTu saldo actual es: "+saldoCuenta+ "\nY el saldo anterior era: "+(saldoCuenta + val1));
+        if(val1 < saldoCuenta){
+            if(val1 < limiteExtraccion){
+                var validar= val1 /100
+                if (validar % 1 == 0){  
+                restarcuenta(val1);
+                alert("Se ha retirado: "+val1 +"\nTu saldo actual es: "+saldoCuenta+ "\nY el saldo anterior era: "+(saldoCuenta + val1));
+                }
+                else{
+                    alert("La extraccion no es valida. Solo se puede entregar billetes de 100");
+                }
+            }
+            else{
+                alert("Se ha superado el limite de extraccion");
+            }
+        }
+        else{
+            alert("no hay saldo disponible para entregar esa cantida de dinero");
+        }
     }
 }
 
